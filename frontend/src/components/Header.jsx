1289ba70cee61e3ced3a1ee282a0e4dd092c53f0
Andrew Havard <rocketboy@thehavards.net>
2020-11-29 14:30:14 -0600

    Readded the header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,33 +3,35 @@ import { NavLink } from 'react-router-dom';
 
 export const Header = props => <>
   <nav className="navbar navbar-dark navbar-expand-md bg-dark collapse navbar-collapse">
-    <ul className="navbar-nav mr-auto">
-      <li className="nav-item">
-        <NavLink className="nav-link" activeClassName="text-white font-weight-bold" to="/soldiers">Soldiers</NavLink>
-      </li>
-      <li className="nav-item">
-        <NavLink className="nav-link" activeClassName="text-white font-weight-bold" to="/notifications">Notifications</NavLink>
-      </li>
-        {props.authentication.authLevel > 1 && 
-          <li className="nav-item">
-            <NavLink className="nav-link" activeClassName="text-white font-weight-bold" to="/personnelManagement">Manage Personnel</NavLink>
-          </li>}
-      <li className="nav-item">
-        <NavLink className="nav-link" activeClassName="text-white font-weight-bold" to="/profile">Profile</NavLink>
-      </li>
-        {!props.authentication.identity &&
-          <li className="nav-item">
-            <NavLink className="nav-link" activeClassName="text-white font-weight-bold" to="/identityCheck">Verify Identity</NavLink>
-          </li>}
-    </ul>
-    <ul className="navbar-nav">
-      <li className="nav-item nav-link text-white font-weight-bold">{props.authentication.username}</li>
-      {props.authentication.loggedIn && 
+    <div className="collapse navbar-collapse">
+      <ul className="navbar-nav mr-auto">
         <li className="nav-item">
-          <button type="button" className="btn btn-secondary m-0"
-            onClick={() => {let newAuth = props.authentication; newAuth.loggedIn = false; props.onAuthChange(newAuth)}}>Sign Out</button>
+          <NavLink className="nav-link" activeClassName="text-white font-weight-bold" to="/soldiers">Soldiers</NavLink>
         </li>
-      }
-    </ul>
+        <li className="nav-item">
+          <NavLink className="nav-link" activeClassName="text-white font-weight-bold" to="/notifications">Notifications</NavLink>
+        </li>
+          {props.authentication.authLevel > 1 && 
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="text-white font-weight-bold" to="/personnelManagement">Manage Personnel</NavLink>
+            </li>}
+        <li className="nav-item">
+          <NavLink className="nav-link" activeClassName="text-white font-weight-bold" to="/profile">Profile</NavLink>
+        </li>
+          {!props.authentication.identity &&
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="text-white font-weight-bold" to="/identityCheck">Verify Identity</NavLink>
+            </li>}
+      </ul>
+      <ul className="navbar-nav">
+        <li className="nav-item nav-link text-white font-weight-bold">{props.authentication.username}</li>
+        {props.authentication.loggedIn && 
+          <li className="nav-item">
+            <button type="button" className="btn btn-secondary m-0"
+              onClick={() => {let newAuth = props.authentication; newAuth.loggedIn = false; props.onAuthChange(newAuth)}}>Sign Out</button>
+          </li>
+        }
+      </ul>
+    </div>
   </nav>
 </>
